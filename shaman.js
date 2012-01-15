7c45a7fae6411df67ba2bed4d2402ae9826a65fc
Miro Bezjak <bezjak.miro@gmail.com>
2012-01-15 11:43:40 +0100

    Nicer code formatting.

--- shaman.js
+++ shaman.js
@@ -49,35 +49,35 @@ shaman.data.write = function(store) {
 };
 
 shaman.link.imdb = function(model) {
-  var link = model.get('imdb');
-  var name = model.get('name');
+  var link   = model.get('imdb');
+  var name   = model.get('name');
   var search = 'http://www.imdb.com/find?s=tt&q={0}';
 
   return link || Ext.String.format(search, name);
 };
 
 shaman.link.wiki = function(model) {
-  var link = model.get('wiki');
-  var name = model.get('name');
+  var link   = model.get('wiki');
+  var name   = model.get('name');
   var search = 'http://en.wikipedia.org/wiki/Special:Search?search={0}';
 
   return link || Ext.String.format(search, name);
 };
 
 shaman.link.btjunkie = function(model) {
-  var name = model.get('name');
-  var season = Ext.String.leftPad(model.get('season'), 2, '0');
+  var name    = model.get('name');
+  var season  = Ext.String.leftPad(model.get('season'), 2, '0');
   var episode = Ext.String.leftPad(model.get('episode'), 2, '0');
-  var search = 'http://btjunkie.org/search?q={0} s{1}e{2}';
+  var search  = 'http://btjunkie.org/search?q={0} s{1}e{2}';
 
   return Ext.String.format(search, name, season, episode);
 };
 
 shaman.link.isohunt = function(model) {
-  var name = model.get('name');
-  var season = Ext.String.leftPad(model.get('season'), 2, '0');
+  var name    = model.get('name');
+  var season  = Ext.String.leftPad(model.get('season'), 2, '0');
   var episode = Ext.String.leftPad(model.get('episode'), 2, '0');
-  var search = 'http://isohunt.com/torrents/?ihq={0} s{1}e{2}';
+  var search  = 'http://isohunt.com/torrents/?ihq={0} s{1}e{2}';
 
   return Ext.String.format(search, name, season, episode);
 };
@@ -91,34 +91,34 @@ shaman.link.open = function(link) {
 };
 
 Ext.define('shaman.Show', {
-  extend: 'Ext.data.Model',
-  idProperty: 'name',
-  fields: [
-    { name: 'name',    type: 'string' },
-    { name: 'group',   type: 'string' },
-    { name: 'season',  type: 'int'    },
-    { name: 'episode', type: 'int'    },
-    { name: 'imdb',    type: 'string' },
-    { name: 'wiki',    type: 'string' }
+  extend     : 'Ext.data.Model',
+  idProperty : 'name',
+  fields     : [
+    { name : 'name',    type : 'string' },
+    { name : 'group',   type : 'string' },
+    { name : 'season',  type : 'int'    },
+    { name : 'episode', type : 'int'    },
+    { name : 'imdb',    type : 'string' },
+    { name : 'wiki',    type : 'string' }
   ]
 });
 
 Ext.define('shaman.Store', {
-  extend: 'Ext.data.Store',
-  model: 'shaman.Show',
-  storeId: 'shows',
-  groupField: 'group',
-  listeners: {
-    add: shaman.data.write,
-    remove: shaman.data.write,
-    update: shaman.data.write
+  extend     : 'Ext.data.Store',
+  model      : 'shaman.Show',
+  storeId    : 'shows',
+  groupField : 'group',
+  listeners  : {
+    add    : shaman.data.write,
+    remove : shaman.data.write,
+    update : shaman.data.write
   }
 });
 
 Ext.define('shaman.Grid', {
-  extend: 'Ext.grid.Panel',
-  title: 'Shaman',
-  columns: [
+  extend   : 'Ext.grid.Panel',
+  title    : 'Shaman',
+  columns  : [
     { header: 'Name',    dataIndex: 'name',    editor: 'textfield'   },
     { header: 'Group',   dataIndex: 'group',   editor: { xtype: 'combobox', forceSelection: true, store: ['active', 'inactive', 'maybe', 'notwatching', 'ended']} },
     { header: 'Season',  dataIndex: 'season',  editor: 'numberfield' },
@@ -153,18 +153,18 @@ Ext.define('shaman.Grid', {
       }]
     }
   ],
-  selType: 'rowmodel',
-  features: [{ ftype: 'grouping' }]
+  selType  : 'rowmodel',
+  features : [{ ftype: 'grouping' }]
 });
 
 Ext.onReady(function() {
 
   var store = new shaman.Store({
-    data: shaman.data.read()
+    data : shaman.data.read()
   });
 
   var rowEditing = Ext.create('Ext.grid.plugin.RowEditing', {
-    clicksToEdit: 2
+    clicksToEdit : 2
   });
 
   var addShow = function() {
@@ -173,20 +173,20 @@ Ext.onReady(function() {
   };
 
   new Ext.util.KeyMap(Ext.getBody(), {
-    key: Ext.EventObject.F9,
-    handler: addShow
+    key     : Ext.EventObject.F9,
+    handler : addShow
   });
 
   var grid = new shaman.Grid({
-    region: 'center',
-    store: store,
-    plugins: rowEditing,
-    tbar: [{
-      text: 'Add Show',
-      handler: addShow
+    region  : 'center',
+    store   : store,
+    plugins : rowEditing,
+    tbar    : [{
+      text    : 'Add Show',
+      handler : addShow
     }, {
-      text: 'Delete Show',
-      handler: function() {
+      text    : 'Delete Show',
+      handler : function() {
         var selected = grid.getSelectionModel().getSelection()[0];
         if (selected) {
           store.remove(selected);
@@ -196,9 +196,9 @@ Ext.onReady(function() {
   });
 
   new Ext.Viewport({
-    layout: 'border',
-    renderTo: Ext.getBody(),
-    items: [grid]
+    layout   : 'border',
+    renderTo : Ext.getBody(),
+    items    : [grid]
   });
 
 });
