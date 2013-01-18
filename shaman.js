95741f8f86a107a52275acab08537a9cd78d3bf9
Miro Bezjak <bezjak.miro@gmail.com>
2013-01-18 21:33:54 +0100

    RELEASE 0.6.1

--- shaman.js
+++ shaman.js
@@ -16,7 +16,7 @@
 
 Ext.ns('shaman', 'shaman.data', 'shaman.link');
 
-shaman.version = '0.6';
+shaman.version = '0.6.1';
 
 shaman.data.dateAsString = function(date) {
   return Ext.Date.format(date, 'Y-m-d');
